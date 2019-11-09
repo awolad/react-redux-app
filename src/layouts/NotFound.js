4ae84c6e3640d5851d98dfe9899d5886db69baff
Awolad Hossain <awolad1122@gmail.com>
2019-11-09 22:35:56 +0600

    not found component fix

--- src/layouts/NotFound.js
+++ src/layouts/NotFound.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 import AdNavBar from '../components/AdNavBar';
+import { fetchCategories } from '../actions/category';
 
 class NotFound extends Component {
   static propTypes = {
     children: PropTypes.node.isRequired,
+    dispatch: PropTypes.func.isRequired,
+    category: PropTypes.object.isRequired,
   };
 
   componentDidMount() {
-    // const { children } = this.props;
+    const { dispatch } = this.props;
+    dispatch(fetchCategories());
   }
 
   render() {
-    const { children } = this.props;
+    const { children, category } = this.props;
     return (
       <>
         <div className="not-found-layout">
-          <AdNavBar />
+          <AdNavBar category={category} />
           {children}
         </div>
       </>
@@ -24,4 +30,12 @@ class NotFound extends Component {
   }
 }
 
-export default NotFound;
+const mapStateToProps = (state) => {
+  const { category } = state;
+
+  return {
+    category,
+  };
+};
+
+export default withRouter(connect(mapStateToProps)(NotFound));
